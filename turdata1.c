7637b0e17ac9972fdfe1cccf20e8f9557c95db6a
GoesDey <gusdekmh7778@gmail.com>
2025-03-17 23:56:59 +0800

    membuat menu tampilan dan merapikan code

--- turdata1.c
+++ turdata1.c
@@ -1,51 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_ITEM 100
+#define MAX 100 // batas jumlah barang yang bisa dimasukkan
 
 typedef struct Item
 {
-    int kode;
+    int kode; 
     char nama[50];
     char bahan[50];
     int jumlah;
     float harga;
 };
 
+// function untuk input barang
 void inputBarang(struct Item *items, int *n){
-    int input;
+    // variabel untuk menampung banyak barang yang dinputkan
+    int input = 0; 
     printf("\t\t\t-- INPUT BARANG --\n\n");
     printf("Banyaknya Barang yang akan anda masukkan : ");
     scanf("%d", &input);
 
-    if(*n + input > MAX_ITEM){
+    // mengecek apakah barang sudah penuh atau belum
+    if(*n + input > MAX){
         printf("Barang Penuh");
         return;
     }
 
-
+    // looping untuk input data barang 
     for (int i = 0; i < input; i++){
-        printf(":: Kode Barang      : [%d] : ", *n+1);
+        printf(":: Kode Barang      : [%d] : ", *n + 1);
         scanf("%d", &items[*n].kode);
-        printf(":: Nama Barang      : [%d] : ", *n+1);
+        printf(":: Nama Barang      : [%d] : ", *n + 1);
         scanf("%s", items[*n].nama);
-        printf(":: Bahan Dasar      : [%d] : ", *n+1);
+        printf(":: Bahan Dasar      : [%d] : ", *n + 1);
         scanf("%s", items[*n].bahan);
-        printf(":: Jumlah           : [%d] : ", *n+1);
+        printf(":: Jumlah           : [%d] : ", *n + 1);
         scanf("%d", &items[*n].jumlah);
-        printf(":: Harga Per Unit   : [%d] : Rp. ", *n+1);
+        printf(":: Harga Per Unit   : [%d] : Rp. ", *n + 1);
         scanf("%f", &items[*n].harga);
         printf("\n");
+        // menambah jumlah barang setelah user menginputkan data barang
         (*n)++;
     }
+    system("cls");
+    printf("%d Data Berhasil Ditambahkan\n", input);
 }
+
+// function untuk menampilkan barang yang sudah diinput
 void tampilBarang(struct Item *items, int n){
+    // mengecek persediaan barang
     if (n == 0){
         printf("Belum ada persediaan barang. Tambahkan terlebih dahulu\n");
         return;
 
     }
+
     printf("\t\t\t-- Persediaan Barang --\n\n");
+    // looping untuk menampilkan barang
     for (int i = 0; i < n; i++){
         printf("Data Barang ke %d\n", i + 1);
         printf("\t:: Kode Barang    : %d\n", items[i].kode);
@@ -60,24 +71,32 @@ void tampilBarang(struct Item *items, int n){
 
 
 int main(){
+    system("cls");
     int n = 0;
-    struct Item items[MAX_ITEM];
+    struct Item items[MAX];
     
     int pil;
     do {
-        printf("+-----------------------------------------------------------------+\n");
-        printf("\t\t TUGAS DATABASE TOKO MEBEL RAMOS\n");
-        printf("+-----------------------------------------------------------------+\n");
-        printf("Masukkan Pilihan :");
+        printf("+===================================================================+\n");
+        printf("                TUGAS DATABASE TOKO MEBEL RAMOS\n");
+        printf("+===================================================================+\n");
+        printf("|                                                                   |\n");        
+        printf("|                        [MENU PILIHAN]                             |\n");        
+        printf("|                        1. Input Barang                            |\n");        
+        printf("|                        2. Lihat Stok Barang                       |\n");        
+        printf("|                        3. Exit                                    |\n");        
+        printf("|                                                                   |\n");        
+        printf("+===================================================================+\n");        
+        printf("                        Masukkan Pilihan : ");
         scanf("%d", &pil);
-
+        
         switch(pil){
             case 1 : {
                 system("cls");
                 inputBarang(items, &n);
             }
-                
             break;
+
             case 2: {
                 system("cls");
                 tampilBarang(items, n);
